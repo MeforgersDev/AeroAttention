eb580e7eeb4901e08d187fb914e692d7f0aa1f15
Meforgers <45572414+MeforgersDev@users.noreply.github.com>
2024-10-02 13:49:27 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="aeroattention",
+    version="1.0.0",  
+    author="MeforgersAI",
+    author_email="aixr@meforgers.com",
+    description="AeroAttention: An Advanced Quantum-Enhanced Attention Mechanism for Transformers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MeforgersDev/AeroAttention",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy>=1.19.5",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov",
+        ],
+    },
+    include_package_data=True,
+    license="Apache License 2.0",
+    keywords="attention mechanism transformer quantum machine-learning",
+    project_urls={
+        "Bug Reports": "https://github.com/MeforgersDev/AeroAttention/issues",
+        "Source": "https://github.com/MeforgersDev/AeroAttention",
+    },
+)
